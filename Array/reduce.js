840cf03ecc342cf32d36ec6db0f4a53fd02f106e
lele <lele18342960091@outlook.com>
2019-09-12 10:21:34 +0800

    Array

---
+++ Array/reduce.js
@@ -0,0 +1,28 @@
+// reduce代替map和filter
+const _arr = [0, 1, 2];
+
+// map
+const arr = _arr.map(v => v * 2);
+const arr = _arr.reduce((t, c) => {
+    t.push(c * 2);
+    return t;
+}, []);
+// arr => [0, 2, 4]
+
+// filter
+const arr = _arr.filter(v => v > 0);
+const arr = _arr.reduce((t, c) => {
+    c > 0 && t.push(c);
+    return t;
+}, []);
+// arr => [1, 2]
+
+// map和filter
+const arr = _arr.map(v => v * 2).filter(v => v > 2);
+const arr = _arr.reduce((t, c) => {
+    c = c * 2;
+    c > 2 && t.push(c);
+    return t;
+}, []);
+// arr => [4]
+
---
+++ Array/过滤空值.js
@@ -0,0 +1,2 @@
+const arr = [undefined, null, "", 0, false, NaN, 1, 2].filter(Boolean);
+// arr => [1, 2]
